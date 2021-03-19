8398d22333018e862a6d9202a9de7d720acba338
smokeytube <67290867+smokeytube@users.noreply.github.com>
2021-03-19 10:35:26 -0400

    Edited how PHP renders HTML to the screen

--- serverlists.php
+++ serverlists.php
@@ -24,21 +24,21 @@
 	<link rel="stylesheet" href="css/animate.css">
 
 	<meta name="title" content="Minecraft Server IPs">
-        <meta name="description" content="">
-        <meta name="theme-color" content="#526935">
-        <meta name="viewport" content="width=device-width, initial-scale=1.0">
-       <!-- Open Graph -->
-        <meta property="og:type" content="website">
-        <meta property="og:url" content="https://wedoaliltrollin.com/">
-        <meta property="og:title" content="Minecraft Server IPs">
-        <meta property="og:description" content="">
-        <meta property="og:image" content="https://wedoaliltrollin.com/images/iplists.png">
-       <!-- Twitter -->
-        <meta property="twitter:card" content="summary_large_image">
-        <meta property="twitter:url" content="https://wedoaliltrollin.com/">
-        <meta property="twitter:title" content="Minecraft Server IPs">
-        <meta property="twitter:description" content="">
-        <meta property="twitter:image" content="https://wedoaliltrollin.com/images/iplists.png">
+	<meta name="description" content="">
+	<meta name="theme-color" content="#526935">
+	<meta name="viewport" content="width=device-width, initial-scale=1.0">
+	<!-- Open Graph -->
+	<meta property="og:type" content="website">
+	<meta property="og:url" content="https://wedoaliltrollin.com/">
+	<meta property="og:title" content="Minecraft Server IPs">
+	<meta property="og:description" content="">
+	<meta property="og:image" content="https://wedoaliltrollin.com/images/iplists.png">
+	<!-- Twitter -->
+	<meta property="twitter:card" content="summary_large_image">
+	<meta property="twitter:url" content="https://wedoaliltrollin.com/">
+	<meta property="twitter:title" content="Minecraft Server IPs">
+	<meta property="twitter:description" content="">
+	<meta property="twitter:image" content="https://wedoaliltrollin.com/images/iplists.png">
 
 
 </head>
@@ -57,70 +57,41 @@
 		</div><!-- End row -->
 
 		<div class="row">
-			<div class="col-md-3 col-sm-6">
-				<div class="features-div">
-					<h4>51.0.0.0 - 51.255.255.255</h4>
-					<?php
-					foreach (new DirectoryIterator("ips/51") as $fileInfo) {
-						if ($fileInfo->isDot()) {
-							continue;
-						}
-						$ipfile = $fileInfo->getFilename();
-						echo "<a href='ips/51/$ipfile'><button type='button' class='ip-button'>$ipfile</button></a>" . "<br>\n";
-						echo "<div style='padding: 2px;'></div>";
+
+
+			<?php
+			$files1 = scandir('ips/');
+			$filesarr = array();
+
+			foreach ($files1 as &$dirs) {
+				if ($dirs == "." or $dirs == "..") {
+				} 
+				else {
+					array_push($filesarr, $dirs);
+				}
+			}
+
+
+			foreach ($filesarr as &$thefiledir) {
+				echo "<div class='col-md-3 col-sm-6'>";
+				echo "<div class='features-div'>";
+				echo "<h4>$thefiledir.0.0.0 - $thefiledir.255.255.255</h4>";	
+				foreach (new DirectoryIterator("ips/$thefiledir") as $fileInfo) {
+					if ($fileInfo->isDot()) {
+						continue;
 					}
-					?>
-				</div>
-			</div><!-- End col-md-3-->
-			<div class="col-md-3 col-sm-6">
-				<div class="features-div">
-					<h4>54.0.0.0 - 54.255.255.255</h4>
-					<?php
-					foreach (new DirectoryIterator("ips/54") as $fileInfo) {
-						if ($fileInfo->isDot()) {
-							continue;
-						}
-						$ipfile = $fileInfo->getFilename();
-						echo "<a href='ips/54/$ipfile'><button type='button' class='ip-button'>$ipfile</button></a>" . "<br>\n";
-						echo "<div style='padding: 2px;'></div>";
-					}
-					?>
-				</div>
-			</div><!-- End col-md-3-->
-			<div class="col-md-3 col-sm-6">
-				<div class="features-div">
-					<h4>147.0.0.0 - 147.255.255.255</h4>
-					<?php
-					foreach (new DirectoryIterator("ips/147") as $fileInfo) {
-						if ($fileInfo->isDot()) {
-							continue;
-						}
-						$ipfile = $fileInfo->getFilename();
-						echo "<a href='ips/147/$ipfile'><button type='button' class='ip-button'>$ipfile</button></a>" . "<br>\n";
-						echo "<div style='padding: 2px;'></div>";
-					}
-					?>
-				</div>
-			</div><!-- End col-md-3-->
-			<div class="col-md-3 col-sm-6">
-				<div class="features-div">
-					<h4>173.0.0.0 - 173.255.255.255</h4>
-					<?php
-					foreach (new DirectoryIterator("ips/173") as $fileInfo) {
-						if ($fileInfo->isDot()) {
-							continue;
-						}
-						$ipfile = $fileInfo->getFilename();
-						echo "<a href='ips/173/$ipfile'><button type='button' class='ip-button'>$ipfile</button></a>" . "<br>\n";
-						echo "<div style='padding: 2px;'></div>";
-					}
-					?>
-				</div>
-			</div><!-- End col-md-3-->
-			<div style='text-align: center;'>
-			<div style="padding: 31%;"></div>
-			<p>New IPs will be added soon. Please be patient</p>
-			</div>
+					$ipfile = $fileInfo->getFilename();
+					echo "<a href='ips/$thefiledir/$ipfile'><button type='button' class='ip-button'>$ipfile</button></a>" . "<br>\n";
+					echo "<div style='padding: 2px;'></div>";
+				}
+				echo "</div>";
+				echo "</div>";
+				
+			}
+
+			?>
+
+
 		</div><!-- End row -->
 	</div><!-- End Container -->
 
